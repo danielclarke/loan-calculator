3ebf3341a14d608b7d6263fd0be5728cc257d7c1
daniel <dan137clarke@gmail.com>
2019-08-16 08:38:03 +1000

    now have random size, position, and colour circles

--- src/BarChart.js
+++ src/BarChart.js
@@ -3,25 +3,39 @@ import * as d3 from 'd3';
 
 
 class BarChart extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {tick: 0};
+    }
 
     componentDidMount() {
         this.drawChart();
     }
 
     drawChart() {
-        const data = this.props.data;
-        d3.select(this.props.container)
-            .selectAll("div")
+        const data = [...Array(25)].map(() => {return Math.random() * 20});
+        const svg = d3.select(this.props.container)
+            .append("svg")
+            .attr("width", this.props.width)
+            .attr("height", this.props.width);
+
+        svg.selectAll("circle")
             .data(data)
             .enter()
-            .append("div")
-            .attr("class", "bar")
-            .style("height", (d) => { return d * 5 + "px" });
-        
+            .append("circle")
+            .attr("cx", (d, i) => {return i * 10})
+            .attr("cy", (d, i) => {return d * 10})
+            .attr("r", () => {return Math.random() * 10})
+            .attr("fill", () => {return `rgba(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255}, ${Math.random()})`});
+    }
+
+    refresh() {
+        setInterval(() => {this.setState({tick: this.state.tick + 1000})}, 1000);
     }
 
     render() {
         this.drawChart();
+        //this.refresh()
         return null;
     }
 }
